b2f0ab4a44878b106748389433e3d19a3f93a20f
Martin JM Watson <martinwatsonja@gmail.com>
2016-09-06 01:32:47 -0500

    Add settings menu
    Settings menu doesn't currently do anything, but it is relevant to the
    course at this stage.  In later lessons it will be fully fleshed out.

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.android.sunshine.app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; adds items to the menu bar if it is present
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks.  The action bar will automatically handle
+        // clicks on the Home/Up button, as long as the parent activity is specified in
+        // the AndroidManifest.xml file.
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
