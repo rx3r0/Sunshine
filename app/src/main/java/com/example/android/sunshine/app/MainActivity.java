d46f14d7e9a1f3bcabb6fd9f18e62a5556d040ad
Martin JM Watson <martinwatsonja@gmail.com>
2016-09-06 01:59:08 -0500

    Create forecast fragment for weather forecast
    Also added app icon asset.

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -11,6 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.container, new ForecastFragment())
+                .commit();
     }
 
     @Override
