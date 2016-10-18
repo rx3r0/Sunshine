c398509fbb56851e7fba876327128c7e03cc1067
Martin JM Watson <martinwatsonja@gmail.com>
2016-10-17 22:03:45 -0500

    Fetch weather using AsyncTask

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -1,14 +1,22 @@
 package com.example.android.sunshine.app;
 
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -49,7 +57,7 @@ public class ForecastFragment extends Fragment {
 
         // Create adapter and initialise adapter with dummy data
         // Adapter takes data from the array list and uses it to populate forecast list view
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
                 getActivity(), R.layout.list_item_forecast, R.id.list_item_forecast_textview,
                 weatherForecastList);
 
@@ -60,6 +68,95 @@ public class ForecastFragment extends Fragment {
         forecastListView.setAdapter(adapter);
 
         return rootView;
+    }
+
+    private class FetchWeatherTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            // These need to be declared outside the try/catch block so that they can be closed in
+            // the finally block.  The connection and buffered stream reader will be initialised later
+            // in the try block.
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            // Contain raw JSON response string
+            String forescastStr = null;
+
+            try {
+                // Construct the URL for Open Weather Map's query.
+                // Possible parameters are available at Open Weather Map's forecast API page:
+                // http://openweathermap.org/API#forecast
+
+                // The URL is too long.  For readability, break it up into smaller pieces and
+                // concatenated it.
+                final String URL_BASE = "http://api.openweathermap.org/data/2.5/forecast/daily";
+                final String LOCATION_QUERY = "?q=spanish%20town,jm";
+                final String MODE = "&mode=json";
+                final String UNITS = "&units=metric";
+                final String COUNT = "&cnt=7";
+                final String API_ID = BuildConfig.OPEN_WEATHER_MAP_API_KEY;
+
+                URL url = new URL(URL_BASE + LOCATION_QUERY + MODE + UNITS + COUNT + API_ID);
+
+                // Create connection to Open Weather Map and open it.
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Create input stream and read into string buffer
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+
+                // If the input stream is null, then there's nothing to do, so return null and end.
+                if (inputStream == null) {
+                    return null;
+                }
+
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                // Break stream into individual lines by appending \n to each line
+                String line;
+
+                while ((line = reader.readLine()) != null) {
+                    // Adding a new line to the JSON data is not necessary as it will not affect the
+                    // parsing.  However, it does make debugging a lot easier if the completed buffer
+                    // is printed.
+                    buffer.append(line + "\n");
+                }
+
+                // If the stream was empty then there's no point in parsing.
+                if (buffer.length() == 0) {
+                    forescastStr = null;
+                }
+
+                forescastStr = buffer.toString();
+
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Error", e);
+
+                // If there was no data returned there is no reason it should be parsed, therefore,
+                // just return null.
+                forescastStr = null;
+            } finally {
+                // If still connected, then disconnect.
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+
+                    // If the stream buffer is still open, then close it.
+                    if (reader != null) {
+                        try {
+                            reader.close();
+                        } catch (IOException e) {
+                            Log.e(LOG_TAG, "Error closing stream", e);
+                        }
+
+                    }
+                }
+            }
+
+            return null;
+        }
     }
 
 }
