a0f11188aa224837a34ea43a5acaaab6a31ddee6
Martin JM Watson <martinwatsonja@gmail.com>
2017-01-05 23:12:57 -0500

    Add list item to layout and fill with fake data

--- app/src/main/java/io/github/rx3r0/sunshine/MainActivityFragment.java
+++ app/src/main/java/io/github/rx3r0/sunshine/MainActivityFragment.java
@@ -1,10 +1,16 @@
 package io.github.rx3r0.sunshine;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,6 +23,31 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        String[] dummyForecast = {
+                "Mon 6/23 - Sunny - 31/17",
+                "Tue 6/24 - Foggy - 21/8",
+                "Wed 6/25 - Cloudy - 22/17",
+                "Thurs 6/26 - Rainy - 18/11",
+                "Fri 6/27 - Foggy - 21/10",
+                "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+                "Sun 6/29 - Sunny - 20/7"
+        };
+
+        List<String> dummyWeekForecast = new ArrayList<>(
+                Arrays.asList(dummyForecast));
+
+        ArrayAdapter<String> forecastAdapter = new ArrayAdapter<String>(
+                getActivity(),
+                R.layout.list_item_forecast,
+                R.id.list_item_forecast_textview,
+                dummyWeekForecast
+        );
+
+        ListView forecastView = (ListView) rootView.findViewById(R.id.listview_forecast);
+        forecastView.setAdapter(forecastAdapter);
+
+        return rootView;
     }
 }
